241bbec5302ac7ddc0192872a5d3cef3b7a95110
Anthony Eden <me@anthonyeden.com>
2016-08-28 19:24:46 +0200

    Implement list domains.

--- test/dnsimple/domains.spec.js
+++ test/dnsimple/domains.spec.js
@@ -1,18 +1,26 @@
 'use strict';
 
 var testUtils = require('../testUtils');
-var dnsimple = require('../../lib/dnsimple')(
-  testUtils.getAccessToken()
-);
+var dnsimple = require('../../lib/dnsimple')({
+  accessToken: testUtils.getAccessToken(),
+});
 
 var expect = require('chai').expect;
+var nock = require('nock');
+var fixture = testUtils.fixture('listDomains/success.http');
+var endpoint = nock('https://api.dnsimple.com')
+                 .get('/v2/1010/domains')
+                 .reply(fixture.statusCode, fixture.body);
 
 describe('domains', function() {
   describe('#listDomains', function() {
-    it('returns a domain list', function() {
+    it('produces a domain list', function(done) {
       var accountId = '1010'
-      var domains = dnsimple.domains.listDomains(accountId);
-      expect(domains).to.eql([]);
+      dnsimple.domains.listDomains(accountId, function(error, response) {
+        expect(error).to.be.null;
+        expect(response.data.length).to.eq(2);
+        done();
+      });
     });
   });
 });
