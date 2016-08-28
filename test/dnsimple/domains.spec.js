9a1020971b068968dfcdea0bb28a62190d96705a
Anthony Eden <me@anthonyeden.com>
2016-08-28 20:42:27 +0200

    Implement get domain.

--- test/dnsimple/domains.spec.js
+++ test/dnsimple/domains.spec.js
@@ -7,13 +7,14 @@ var dnsimple = require('../../lib/dnsimple')({
 
 var expect = require('chai').expect;
 var nock = require('nock');
-var fixture = testUtils.fixture('listDomains/success.http');
-var endpoint = nock('https://api.dnsimple.com')
-                 .get('/v2/1010/domains')
-                 .reply(fixture.statusCode, fixture.body);
 
 describe('domains', function() {
   describe('#listDomains', function() {
+    var fixture = testUtils.fixture('listDomains/success.http');
+    var endpoint = nock('https://api.dnsimple.com')
+                     .get('/v2/1010/domains')
+                     .reply(fixture.statusCode, fixture.body);
+
     it('produces a domain list', function(done) {
       var accountId = '1010'
       dnsimple.domains.listDomains(accountId, function(error, response) {
@@ -25,5 +26,37 @@ describe('domains', function() {
         done();
       });
     });
+  });
+
+  describe('#domain', function() {
+    var fixture = testUtils.fixture('getDomain/success.http');
+    var endpoint = nock('https://api.dnsimple.com')
+                     .get('/v2/1010/domains/example-alpha.com')
+                     .reply(fixture.statusCode, fixture.body);
+
+    it('produces a domain', function(done) {
+      var accountId = '1010'
+      var domainId = 'example-alpha.com';
+      dnsimple.domains.domain(accountId, domainId, function(error, response) {
+        expect(error).to.be.null;
+        var domain = response.data;
+        expect(domain.id).to.eq(1);
+        expect(domain.account_id).to.eq(1010);
+        expect(domain.registrant_id).to.be.null;
+        expect(domain.name).to.eq('example-alpha.com');
+        expect(domain.state).to.eq('hosted');
+        expect(domain.auto_renew).to.eq(false);
+        expect(domain.private_whois).to.eq(false);
+        expect(domain.expires_on).to.be.null;
+        expect(domain.created_at).to.eq('2014-12-06T15:56:55.573Z');
+        expect(domain.updated_at).to.eq('2015-12-09T00:20:56.056Z');
+        done();
+      });
+    });
+
+    describe('when the domain does not exist', function() {
+
+    });
+
   });
 });
