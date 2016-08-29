69e6e0f5d0639890261190be9a86face2de460e8
Anthony Eden <me@anthonyeden.com>
2016-08-29 16:18:13 +0200

    Implement renew, transfer and transfer out domain.

--- test/dnsimple/registrar.spec.js
+++ test/dnsimple/registrar.spec.js
@@ -53,4 +53,119 @@ describe('registrar', function() {
       });
     });
   });
+
+  describe('#renewDomain', function() {
+    var fixture = testUtils.fixture('renewDomain/success.http');
+
+    it('produces a domain', function(done) {
+      var attributes = {period: '3'};
+
+      nock('https://api.dnsimple.com')
+        .post('/v2/1010/registrar/domains/example.com/renewal', attributes)
+        .reply(fixture.statusCode, fixture.body);
+
+      dnsimple.registrar.renewDomain(accountId, domainId, attributes).then(function(response) {
+        var domain = response.data;
+        expect(domain.id).to.eq(1);
+        expect(domain.name).to.eq('example.com');
+        expect(domain.state).to.eq('registered');
+        done();
+      }, function(error) {
+        done(error);
+      });
+    });
+
+    describe('when it is too soon for the domain to be renewed', function() {
+      var fixture = testUtils.fixture('renewDomain/error-tooearly.http');
+
+      it('results in an error', function(done) {
+        var attributes = {};
+
+        nock('https://api.dnsimple.com')
+          .post('/v2/1010/registrar/domains/example.com/renewal', attributes)
+          .reply(fixture.statusCode, fixture.body);
+
+        dnsimple.registrar.renewDomain(accountId, domainId, attributes).then(function(response) {
+          done('Expected error, but future resolved');
+        }, function(error) {
+          expect(error).to.not.be.null;
+          done();
+        });
+      });
+    });
+  });
+
+  describe('#transferDomain', function() {
+    var attributes = {registrant_id: '10', auth_info: 'x1y2z3'};
+
+    it('produces a domain', function(done) {
+      var fixture = testUtils.fixture('transferDomain/success.http');
+      nock('https://api.dnsimple.com')
+        .post('/v2/1010/registrar/domains/example.com/transfer', attributes)
+        .reply(fixture.statusCode, fixture.body);
+
+      dnsimple.registrar.transferDomain(accountId, domainId, attributes).then(function(response) {
+        var domain = response.data;
+        expect(domain.id).to.eq(1);
+        expect(domain.name).to.eq('example.com');
+        expect(domain.state).to.eq('hosted');
+        done();
+      }, function(error) {
+        done(error);
+      });
+    });
+
+    describe('when the domain is already in DNSimple', function() {
+      var fixture = testUtils.fixture('transferDomain/error-isdnsimple.http');
+
+      it('results in an error', function(done) {
+        nock('https://api.dnsimple.com')
+          .post('/v2/1010/registrar/domains/example.com/transfer', attributes)
+          .reply(fixture.statusCode, fixture.body);
+
+        dnsimple.registrar.transferDomain(accountId, domainId, attributes).then(function(response) {
+          done('Expected error, but future resolved');
+        }, function(error) {
+          expect(error).to.not.be.null;
+          done();
+        });
+      });
+    });
+
+    describe('when auth info was not provided and is required by the TLD', function() {
+      var fixture = testUtils.fixture('transferDomain/error-missing-authcode.http');
+
+      it('results in an error', function(done) {
+        var attributes = {registrant_id: '10'};
+
+        nock('https://api.dnsimple.com')
+          .post('/v2/1010/registrar/domains/example.com/transfer', attributes)
+          .reply(fixture.statusCode, fixture.body);
+
+        dnsimple.registrar.transferDomain(accountId, domainId, attributes).then(function(response) {
+          done('Expected error, but future resolved');
+        }, function(error) {
+          expect(error).to.not.be.null;
+          done();
+        });
+      });
+    });
+  });
+
+  describe('#transferDomainOut', function() {
+    var fixture = testUtils.fixture('transferDomainOut/success.http');
+
+    it('produces nothing', function(done) {
+      nock('https://api.dnsimple.com')
+        .post('/v2/1010/registrar/domains/example.com/transfer_out')
+        .reply(fixture.statusCode, fixture.body);
+
+      dnsimple.registrar.transferDomainOut(accountId, domainId).then(function(response) {
+        expect(response).to.eql({});
+        done();
+      }, function(error) {
+        done(error);
+      });
+    });
+  });
 });
