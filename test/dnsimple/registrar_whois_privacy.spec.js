2adb1c2baa716db83e1f383cd3fdc41c7fa09bd9
Anthony Eden <me@anthonyeden.com>
2016-09-03 23:08:46 +0200

    Implement registrar whois privacy methods

---
+++ test/dnsimple/registrar_whois_privacy.spec.js
@@ -0,0 +1,96 @@
+'use strict';
+
+var testUtils = require('../testUtils');
+var dnsimple = require('../../lib/dnsimple')({
+  accessToken: testUtils.getAccessToken(),
+});
+
+const expect = require('chai').expect;
+const nock = require('nock');
+
+describe('whois privacy', function() {
+  let accountId = '1010';
+  let domainId = 'example.com';
+
+  describe('#getWhoisPrivacy', function() {
+    var fixture = testUtils.fixture('getWhoisPrivacy/success.http');
+
+    it('produces a whois privacy', function(done) {
+      nock('https://api.dnsimple.com')
+        .get('/v2/1010/registrar/domains/example.com/whois_privacy')
+        .reply(fixture.statusCode, fixture.body);
+
+      dnsimple.registrar.getWhoisPrivacy(accountId, domainId).then(function(response) {
+        var whoisPrivacy = response.data;
+        expect(whoisPrivacy.id).to.eq(1);
+        expect(whoisPrivacy.domain_id).to.eq(2);
+        expect(whoisPrivacy.expires_on).to.eq('2017-02-13');
+        expect(whoisPrivacy.enabled).to.be.true;
+        expect(whoisPrivacy.created_at).to.eq('2016-02-13T14:34:50.135Z');
+        expect(whoisPrivacy.updated_at).to.eq('2016-02-13T14:34:52.571Z');
+        done();
+      }, function(error) {
+        done(error);
+      });
+    });
+  });
+
+  describe('#enableWhoisPrivacy', function() {
+    describe('when whois privacy is already purchased', function() {
+      var fixture = testUtils.fixture('enableWhoisPrivacy/success.http');
+
+      it('produces a whois privacy', function(done) {
+        nock('https://api.dnsimple.com')
+          .put('/v2/1010/registrar/domains/example.com/whois_privacy')
+          .reply(fixture.statusCode, fixture.body);
+
+        dnsimple.registrar.enableWhoisPrivacy(accountId, domainId).then(function(response) {
+          var whoisPrivacy = response.data;
+          expect(whoisPrivacy.id).to.eq(1);
+          expect(whoisPrivacy.domain_id).to.eq(2);
+          done();
+        }, function(error) {
+          done(error);
+        });
+      });
+    });
+
+    describe('when whois privacy is newly purchased', function() {
+      var fixture = testUtils.fixture('enableWhoisPrivacy/created.http');
+
+      it('produces a whois privacy', function(done) {
+        nock('https://api.dnsimple.com')
+          .put('/v2/1010/registrar/domains/example.com/whois_privacy')
+          .reply(fixture.statusCode, fixture.body);
+
+        dnsimple.registrar.enableWhoisPrivacy(accountId, domainId).then(function(response) {
+          var whoisPrivacy = response.data;
+          expect(whoisPrivacy.id).to.eq(1);
+          expect(whoisPrivacy.domain_id).to.eq(2);
+          done();
+        }, function(error) {
+          done(error);
+        });
+      });
+    });
+  });
+
+  describe('#disableWhoisPrivacy', function() {
+    var fixture = testUtils.fixture('disableWhoisPrivacy/success.http');
+
+    it('produces a whois privacy', function(done) {
+      nock('https://api.dnsimple.com')
+        .delete('/v2/1010/registrar/domains/example.com/whois_privacy')
+        .reply(fixture.statusCode, fixture.body);
+
+      dnsimple.registrar.disableWhoisPrivacy(accountId, domainId).then(function(response) {
+        var whoisPrivacy = response.data;
+        expect(whoisPrivacy.id).to.eq(1);
+        expect(whoisPrivacy.domain_id).to.eq(2);
+        done();
+      }, function(error) {
+        done(error);
+      });
+    });
+  });
+});
