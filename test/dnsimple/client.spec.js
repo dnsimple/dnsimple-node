84c2cb9eefedc92866606909b99e8ac1065076f5
Anthony Eden <me@anthonyeden.com>
2016-09-08 18:57:49 +0200

    Use the client directly

--- test/dnsimple/client.spec.js
+++ test/dnsimple/client.spec.js
@@ -1,9 +1,11 @@
 'use strict';
 
-var testUtils = require('../testUtils');
-var dnsimple = require('../../lib/dnsimple')({
+const testUtils = require('../testUtils');
+const dnsimple = require('../../lib/dnsimple')({
   accessToken: testUtils.getAccessToken(),
 });
+
+const Client = require('../../lib/dnsimple/client');
 
 const expect = require('chai').expect;
 const nock = require('nock');
@@ -11,12 +13,12 @@ const nock = require('nock');
 describe('response handling', function() {
   describe('identity.whoami returns a malformed JSON response', function() {
     it('produces an error', function(done) {
-      var fixture = testUtils.fixture('badgateway.http');
+      let fixture = testUtils.fixture('badgateway.http');
       nock('https://api.dnsimple.com')
-        .get('/v2/whoami')
+        .get('/v2/badgateway')
         .reply(fixture.statusCode, fixture.body);
 
-      dnsimple.identity.whoami().then(function(response) {
+      new Client(dnsimple).get('/badgateway', {}).then(function(response) {
         done('Expected error but promise resolved');
       }, function(error) {
         expect(error).to.eq('Unexpected token < in JSON at position 0');
