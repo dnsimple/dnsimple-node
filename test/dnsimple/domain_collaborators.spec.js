e93745bd6e0af32dc70c40caedfe0e796f0775cd
Anthony Eden <me@anthonyeden.com>
2017-03-28 06:00:43 -0500

    Move collaborators into the client.domains namespace.

--- test/dnsimple/domain_collaborators.spec.js
+++ test/dnsimple/domain_collaborators.spec.js
@@ -19,7 +19,7 @@ describe('collaborators', function() {
         .get('/v2/1010/domains/example.com/collaborators?page=1')
         .reply(fixture.statusCode, fixture.body);
 
-      dnsimple.collaborators.listCollaborators(accountId, domainId, {page: 1});
+      dnsimple.domains.listCollaborators(accountId, domainId, {page: 1});
 
       endpoint.done();
       done();
@@ -30,7 +30,7 @@ describe('collaborators', function() {
         .get('/v2/1010/domains/example.com/collaborators?foo=bar')
         .reply(fixture.statusCode, fixture.body);
 
-      dnsimple.collaborators.listCollaborators(accountId, domainId, {query: {foo: 'bar'}});
+      dnsimple.domains.listCollaborators(accountId, domainId, {query: {foo: 'bar'}});
 
       endpoint.done();
       done();
@@ -41,7 +41,7 @@ describe('collaborators', function() {
         .get('/v2/1010/domains/example.com/collaborators')
         .reply(fixture.statusCode, fixture.body);
 
-      dnsimple.collaborators.listCollaborators(accountId, domainId).then(function(response) {
+      dnsimple.domains.listCollaborators(accountId, domainId).then(function(response) {
         var collaborators = response.data;
         expect(collaborators.length).to.eq(2);
         expect(collaborators[0].id).to.eq(100);
@@ -58,7 +58,7 @@ describe('collaborators', function() {
         .get('/v2/1010/domains/example.com/collaborators')
         .reply(fixture.statusCode, fixture.body);
 
-      dnsimple.collaborators.listCollaborators(accountId, domainId).then(function(response) {
+      dnsimple.domains.listCollaborators(accountId, domainId).then(function(response) {
         var pagination = response.pagination;
         expect(pagination).to.not.be.null;
         expect(pagination.current_page).to.eq(1);
@@ -83,7 +83,7 @@ describe('collaborators', function() {
         .post('/v2/1010/domains/example.com/collaborators')
         .reply(fixture.statusCode, fixture.body);
 
-      dnsimple.collaborators.addCollaborator(accountId, domainId, collaborator).then(function(response) {
+      dnsimple.domains.addCollaborator(accountId, domainId, collaborator).then(function(response) {
         var data = response.data;
         expect(data.id).to.eql(100);
         expect(data.invitation).to.eql(false);
@@ -106,7 +106,7 @@ describe('collaborators', function() {
         .delete('/v2/1010/domains/example.com/collaborators/100')
         .reply(fixture.statusCode, fixture.body);
 
-      dnsimple.collaborators.removeCollaborator(accountId, domainId, collaboratorId).then(function(response) {
+      dnsimple.domains.removeCollaborator(accountId, domainId, collaboratorId).then(function(response) {
         expect(response).to.eql({});
         done();
       }, function(error) {
