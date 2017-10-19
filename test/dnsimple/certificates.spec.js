41835c262e5ff92773b6f3bd4c18fa9f67ac7a92
Anthony Eden <me@anthonyeden.com>
2017-10-19 12:44:53 -0400

    Let's Encrypt certificate purchase and issue.
    This commit only handles the happy path. Additional tests are required
    for error cases, especially errors due to the feature not being enabled
    on an account.

--- test/dnsimple/certificates.spec.js
+++ test/dnsimple/certificates.spec.js
@@ -234,4 +234,94 @@ describe('certificates', function() {
       });
     });
   });
+
+  describe('#letsencryptPurchase', function() {
+    var accountId = '1010';
+    var domainId = 'example.com';
+    var attributes = {contact_id: 1}
+    var fixture = testUtils.fixture('purchaseLetsencryptCertificate/success.http');
+
+    it('purchases a certificate', function(done) {
+      nock('https://api.dnsimple.com')
+        .post('/v2/1010/domains/example.com/certificates/letsencrypt')
+        .reply(fixture.statusCode, fixture.body);
+
+      dnsimple.certificates.letsencryptPurchase(accountId, domainId, attributes).then(function(response) {
+        var certificate = response.data;
+        expect(certificate.id).to.eq(200);
+        done();
+      }, function(error) {
+        done(error);
+      });
+    });
+  });
+
+  describe('#letsencryptIssue', function() {
+    var accountId = '1010';
+    var domainId = 'example.com';
+    var certificateId = 200;
+    var fixture = testUtils.fixture('issueLetsencryptCertificate/success.http');
+
+    it('issues a certificate', function(done) {
+      nock('https://api.dnsimple.com')
+        .post('/v2/1010/domains/example.com/certificates/200/issue')
+        .reply(fixture.statusCode, fixture.body);
+
+      dnsimple.certificates.letsencryptIssue(accountId, domainId, certificateId).then(function(response) {
+        var certificate = response.data;
+        expect(certificate.id).to.eq(200);
+        done();
+      }, function(error) {
+        done();
+      });
+    });
+  });
+
+  describe('#letsencryptPurchaseRenewal', function() {
+    var accountId = '1010';
+    var domainId = 'example.com';
+    var certificateId = 200
+    var fixture = testUtils.fixture('purchaseRenewalLetsencryptCertificate/success.http');
+
+    it('purchases a certificate renewal', function(done) {
+      nock('https://api.dnsimple.com')
+        .post('/v2/1010/domains/example.com/certificates/letsencrypt/200/renewal')
+        .reply(fixture.statusCode, fixture.body);
+
+      dnsimple.certificates.letsencryptPurchaseRenewal(accountId, domainId, certificateId)
+          .then(function(response) {
+        var certificateRenewal = response.data;
+        expect(certificateRenewal.id).to.eq(999);
+        expect(certificateRenewal.old_certificate_id).to.eq(certificateId);
+        expect(certificateRenewal.new_certificate_id).to.eq(300);
+        done();
+      }, function(error) {
+        done(error);
+      });
+    });
+  });
+
+  describe('#letsencryptIssueRenewal', function() {
+    var accountId = '1010';
+    var domainId = 'example.com';
+    var oldCertificateId = 200;
+    var certificateRenewalId = 999
+    var fixture = testUtils.fixture('issueRenewalLetsencryptCertificate/success.http');
+
+    it('issues a certificate renewal', function(done) {
+      nock('https://api.dnsimple.com')
+        .post('/v2/1010/domains/example.com/certificates/200/renewals/999/issue')
+        .reply(fixture.statusCode, fixture.body);
+
+      dnsimple.certificates.letsencryptIssueRenewal(accountId, domainId, oldCertificateId, certificateRenewalId)
+          .then(function(response) {
+        var certificateRenewal = response.data;
+        expect(certificateRenewal.id).to.eq(999);
+        done();
+      }, function(error) {
+        done();
+      });
+    });
+
+  });
 });
