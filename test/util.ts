1666e29083da20672f3fab5c0388e35e1f67c860
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2024-03-10 08:58:10 +0100

    Refactor: simplify utils module
    - Use `function` for better semantics
    - Extract smaller functions to provide better semantics at the cost of iterating the fixture lines twice

--- test/util.ts
+++ test/util.ts
@@ -1,31 +1,43 @@
-import * as fs from "fs";
-import type { ReplyFnResult } from "nock/types";
+import { readFileSync } from "fs";
 import { DNSimple } from "../lib/main";
 
-export const createTestClient = () =>
-  new DNSimple({
+function hasJsonContent(lines: string[]) {
+  for (let line of lines) {
+    const [key, value] = line.split(/:\s/);
+    if (key.toLowerCase() === "content-type" && value.includes("application/json"))
+      return true;
+  }
+  return false;
+}
+
+function parseStatusCode(lines: string[]) {
+  return parseInt((lines[0].split(/\s+/))[1], 10);
+}
+
+function parseBody(lines: string[]) {
+  const separatorLineIndex = lines.findIndex((line) => line.trim() === "");
+
+  const rawBody = lines.slice(separatorLineIndex + 1).join("\n").trim();
+
+  if (hasJsonContent(lines) && rawBody !== "")
+    return JSON.parse(rawBody);
+
+  return rawBody;
+}
+
+export function createTestClient() {
+  return new DNSimple({
     accessToken: process.env["TOKEN"] ?? "bogus",
   });
+}
 
-export function readFixtureAt(path: string): () => ReplyFnResult {
-  const data = fs.readFileSync(`${__dirname}/fixtures.http/${path}`, "utf-8");
+export function readFixtureAt(path: string) {
+  const data = readFileSync(`${__dirname}/fixtures.http/${path}`, "utf-8");
   const lines = data.split(/\r?\n/);
 
-  const statusLine = lines.shift()!;
-  const statusParts = statusLine.split(/\s+/);
-  const statusCode = Number.parseInt(statusParts[1], 10);
+  const statusCode = parseStatusCode(lines);
+  if (statusCode === 204)
+    return () => [204, ""];
 
-  const headers: { [name: string]: string } = {};
-  let val;
-  while ((val = lines.shift())) {
-    const pair = val.split(/:\s/);
-    headers[pair[0]] = pair[1];
-  }
-
-  if (statusCode === 204) return () => [204, ""];
-
-  if (headers["Content-Type"] === "application/json")
-    return () => [statusCode, JSON.parse(lines.join("\n"))];
-
-  return () => [statusCode, lines.join("\n")];
+  return () => [statusCode, parseBody(lines)];
 }
