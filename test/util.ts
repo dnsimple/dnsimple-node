f76d82f3a91a9fbccec55a4191c6cf50d21e3591
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2024-03-10 08:23:39 +0100

    Cleanup: remove unused method

--- test/util.ts
+++ test/util.ts
@@ -7,37 +7,6 @@ export const createTestClient = () =>
     accessToken: process.env["TOKEN"] ?? "bogus",
   });
 
-export const loadFixture = (path: string) => {
-  const data = fs.readFileSync(`${__dirname}/fixtures.http/${path}`, "utf-8");
-  const lines = data.split(/\r?\n/);
-
-  const statusLine = lines.shift()!;
-  const statusParts = statusLine.split(/\s+/);
-  const httpVersion = statusParts[0];
-  const statusCode = Number.parseInt(statusParts[1], 10);
-  const reasonPhrase = statusParts[2];
-
-  const headers: { [name: string]: string } = {};
-  let val;
-  while ((val = lines.shift())) {
-    const pair = val.split(/:\s/);
-    headers[pair[0]] = pair[1];
-  }
-
-  return {
-    httpVersion,
-    statusCode,
-    headers,
-    reasonPhrase,
-    body:
-      statusCode === 204
-        ? null
-        : headers["Content-Type"] === "application/json"
-          ? JSON.parse(lines.join("\n"))
-          : lines.join("\n"),
-  };
-};
-
 export function readFixtureAt(path: string): () => ReplyFnResult {
   const data = fs.readFileSync(`${__dirname}/fixtures.http/${path}`, "utf-8");
   const lines = data.split(/\r?\n/);
