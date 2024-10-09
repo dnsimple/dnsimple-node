e5f816095a01b55a56a82d55bcf700b18919b7c4
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2024-10-09 15:57:05 +0200

    Change: restore payload assertions in all PATCH requests

--- test/zone_records.spec.ts
+++ test/zone_records.spec.ts
@@ -199,7 +199,7 @@ describe("zone records", () => {
 
       await dnsimple.zones.updateZoneRecord(accountId, zoneId, recordId, attributes);
 
-      expect(fetchMock.calls()).not.toEqual([]);
+      expect(fetchMock.calls()[0][1]!.body).toEqual(JSON.stringify(attributes));
     });
 
     it("produces a record", async () => {
